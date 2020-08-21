98fd2a90881f2f5d27fd869e03fbf3d10c2cd818
Sisi Freeley <liusi33@gmail.com>
2020-08-21 13:04:15 -0500

    enemies-list problem

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,5 +1,57 @@
 function main() {
-  // Put your code here
+  const enemies = [
+    {
+      FirstName: "Joshua",
+      LastName: "Flowers",
+      Offenses: {
+        reason1: "Being a jerk to me in elementary school",
+        reason2: "Not being nice to me in elementary school"
+      },
+      IsReallyHated: true
+    },
+    {
+      FirstName: "Darth",
+      LastName: "Vader",
+      Offenses: {
+        reason1: "Cut off Luke's hand",
+        reason2: "Murdered all those kids",
+        reason3: "Unkind management practices"
+      },
+      IsReallyHated: false
+    },
+    {
+      FirstName: "Betty",
+      LastName: "Rudelady",
+      Offenses: {
+        reason1: "Phone calls in the theater",
+        reason2: "Phone calls on the bus",
+        reason3: "Phone calls in line at the grocery store",
+        reason4: "Poor conversationalist"
+      },
+      IsReallyHated: true
+    },
+    {
+      FirstName: "Leon",
+      LastName: "Peck",
+      Offenses: {
+        reason1: "Keeps giving me a hotplate"
+      },
+      IsReallyHated: false
+    }
+
+  ]
+
+  console.log("My Enemies List!");
+  console.log("----------------");
+
+  for (let enemy of enemies) {
+    if (enemy.IsReallyHated) {
+      console.log(`${enemy.FirstName} ${enemy.LastName} (Really, really dislike!)`);
+    } else {
+      console.log(`${enemy.FirstName} ${enemy.LastName}`);
+    }
+
+  }
 
 }
 
--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,5 +1,39 @@
 function main() {
-  // Put your code here
+  //array of names
+  const names = ["Nashville",
+    "Hong Kong",
+    "The back yard",
+    "Earth",
+    "London",
+    "The mall",
+    "Ryman Auditorium",
+    "The Great Wall of China"]
+
+
+  console.log("All Place Names");
+  for (let name of names) {
+    console.log(name);
+  }
+
+  console.log("\n");
+
+
+  //console.log(place names with "The")
+  console.log("'The' Place Names");
+  const includeThe = "the"
+  names.filter(name => {
+    if (name.toLowerCase().includes(includeThe.toLowerCase())) {
+      console.log(name);
+    }
+  })
+
+  //another way console.log(place names with "The")
+  names.filter(name => {
+    if (name.startsWith('The')) {
+      console.log(name);
+    }
+  })
+
 
 }
 
